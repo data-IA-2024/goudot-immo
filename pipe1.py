86a487964470e33cd801a10e8b81b4737d552a50
goudot <Emmanuel.Goudot@gmail.com>
2025-10-20 15:46:46 +0200

    Ajout pipe1

--- pipe1.py
+++ pipe1.py
@@ -9,14 +9,19 @@ lecture des txt et écriture csv
 fn=sys.argv[1] # "data/ValeursFoncieres-2025-S1.txt"
 
 # lecture du source, selection de colonnes
-cols='Date mutation|Nature mutation|Valeur fonciere|Code postal|Commune|Surface Carrez du 1er lot|Code type local|Type local|Nombre pieces principales|Surface terrain'.split('|')
-names='date|nature|valeur|cp|commune|surface|code_type|local|pieces|terrain'.split('|')
-df = pd.read_csv(fn, sep='|', usecols=cols, dtype={'Valeur fonciere':np.float64, 'Code postal':str}, decimal=',')
+cols='Date mutation|Nature mutation|Valeur fonciere|No voie|Type de voie|Voie|Code postal|Commune|Surface Carrez du 1er lot|Surface Carrez du 2eme lot|Surface Carrez du 3eme lot|Surface Carrez du 4eme lot|Surface Carrez du 5eme lot|Code type local|Type local|Nombre pieces principales|Surface terrain'.split('|')
+names='date|nature|valeur|no_voie|type_voie|voie|cp|commune|surface1|surface2|surface3|surface4|surface5|code_type|local|pieces|terrain'.split('|')
+df = pd.read_csv(fn, sep='|', usecols=cols, dtype={'Date mutation':str, 'Valeur fonciere':np.float64, 'Code postal':str}, decimal=',')
 df.columns=names
 
-df=df[df.nature=='Vente']
-df=df[~np.isnan(df.code_type)]
-df.fillna(0.0, inplace=True)
+df=df[df.nature=='Vente'] # ne garde que les ventes
+df=df[~np.isnan(df.code_type)] # ne garde que les typa 1/2/3/4
+df.fillna(0.0, inplace=True) # remplis avec zéro
+df.drop_duplicates(subset=['date', 'no_voie', 'type_voie', 'voie', 'cp'], keep=False, inplace=True)
+df['surface_total']=df.surface1+df.surface2+df.surface3+df.surface4+df.surface5
+df['prixm2']=df.valeur/df.surface_total
+
+
 print(df)
 print(df.dtypes)
 
