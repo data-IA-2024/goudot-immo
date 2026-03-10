0acd67b6624c99bb97ec378e2b84686f5f276e18
goudot <Emmanuel.Goudot@gmail.com>
2026-03-10 10:22:13 +0100

    UPD agent, ajout geocode (data.geopf.fr)

--- runIA.py
+++ runIA.py
@@ -7,6 +7,8 @@ from langchain.tools import tool
 
 from dataclasses import dataclass
 
+import requests, json
+
 load_dotenv()
 
 model = ChatDeepSeek(
@@ -32,10 +34,29 @@ def estimate(bien: Bien):
     """
     return f"estimation de votre {bien.type} : 100000€"
 
+@tool
+def geocode(bien:Bien):
+    """
+    geocode address of real estate
+    :param bien:
+    :return: array of [lat, lon]
+    """
+    URL="https://data.geopf.fr/geocodage/search/"
+    print(f">geocode {bien=}")
+    res = requests.get(URL, params={'q': bien.address, 'limit':2, 'autocomplete':1})
+    #print(json.dumps(res.json(), indent=2))
+    return res.json().get('features')[0].get('geometry').get('coordinates')
+
+'''
+bien = Bien(address="Rue Matabiau, Toulouse", type='house', surface=100)
+pos = geocode(bien)
+print(f"{pos=}")
+'''
+
 
 agent = create_agent(
     model=model,
-    tools=[estimate],
+    tools=[estimate, geocode],
     system_prompt="You are a helpful assistant",
 )
 
@@ -44,9 +65,9 @@ response = agent.invoke(
         "messages": [
             {
                 "role": "user",
-                "content": "estime le prix de ma maison située"
-                           " au centre de Blois,"
-                           " elle fait 100m2",
+                "content": "estime le prix et la position de ma maison située"
+                " 10 rue Matabiau, face à la gare de Toulouse,"
+                " elle fait 100m2",
             }
         ]
     }
